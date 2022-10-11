cd23082a0849a78957591a75d13e17fcad64d26d
boubou19 <miisterunknown@gmail.com>
2022-10-11 23:36:20 +0200

    added buildscript

--- src/main/java/ru/fewizz/idextender/IDExtender.java
+++ src/main/java/ru/fewizz/idextender/IDExtender.java
@@ -3,9 +3,13 @@ package ru.fewizz.idextender;
 import cpw.mods.fml.common.*;
 import cpw.mods.fml.common.event.*;
 
-@Mod(modid = "notenoughIDs", name = "NotEnoughIDs", version = "1.4.3.4")
+@Mod(modid = IDExtender.MODID, name = IDExtender.NAME, version = IDExtender.VERSION)
 public class IDExtender
 {
+    public static final String MODID = "GRADLETOKEN_MODID";
+    public static final String VERSION = "GRADLETOKEN_VERSION";
+    public static final String NAME = "GRADLETOKEN_MODNAME";
+    
     @Mod.EventHandler
     public void test(final FMLPreInitializationEvent event) {
     }
