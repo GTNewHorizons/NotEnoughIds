434a092a247f01df5c6e904b6e189bce444a81fa
Caedis <Caedis@users.noreply.github.com>
2023-11-17 12:49:23 -0600

    spotless

--- src/main/java/ru/fewizz/idextender/mixins/early/minecraft/MixinBlockFire.java
+++ src/main/java/ru/fewizz/idextender/mixins/early/minecraft/MixinBlockFire.java
@@ -5,6 +5,7 @@ import net.minecraft.block.BlockFire;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
 import ru.fewizz.idextender.asm.Constants;
 
 @Mixin(BlockFire.class)
