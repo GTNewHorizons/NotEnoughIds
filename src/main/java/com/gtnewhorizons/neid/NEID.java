2bd63322c41040206a8f0603dfe651875833c300
Darren Eberly <darren@eber.ly>
2023-11-24 22:52:54 -0500

    Add minimum version for battlegear2 compat

--- src/main/java/com/gtnewhorizons/neid/NEID.java
+++ src/main/java/com/gtnewhorizons/neid/NEID.java
@@ -2,6 +2,6 @@ package com.gtnewhorizons.neid;
 
 import cpw.mods.fml.common.Mod;
 
-@Mod(modid = "neid", name = "NotEnoughIDs", version = Tags.VERSION)
+@Mod(modid = "neid", name = "NotEnoughIDs", version = Tags.VERSION, dependencies = "after:battlegear2@[1.3.0,);")
 public class NEID {
 }
