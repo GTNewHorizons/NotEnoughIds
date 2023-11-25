41001b13ceebce8c8ec229677f0778cf61e967c1
Caedis <Caedis@users.noreply.github.com>
2023-11-24 21:12:09 -0600

    Change to static mod id and mod name and only have the version be dynamic

--- src/main/java/com/gtnewhorizons/neid/NEID.java
+++ src/main/java/com/gtnewhorizons/neid/NEID.java
@@ -2,11 +2,5 @@ package com.gtnewhorizons.neid;
 
 import cpw.mods.fml.common.Mod;
 
-@Mod(modid = NEID.MODID, name = NEID.NAME, version = NEID.VERSION)
-public class NEID {
-
-    public static final String MODID = "GRADLETOKEN_MODID";
-    public static final String VERSION = "GRADLETOKEN_VERSION";
-    public static final String NAME = "GRADLETOKEN_MODNAME";
-
-}
+@Mod(modid = "neid", name = "NotEnoughIDs", version = Tags.VERSION)
+public class NEID {}
