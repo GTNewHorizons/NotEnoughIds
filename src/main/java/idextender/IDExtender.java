8946648777965fcde30760522dbaea184bb6fef6
Darren Eberly <darren.eberly@gmail.com>
2024-07-19 21:00:32 -0400

    spotless

--- src/main/java/idextender/IDExtender.java
+++ src/main/java/idextender/IDExtender.java
@@ -2,11 +2,7 @@ package idextender;
 
 import cpw.mods.fml.common.Mod;
 
-@Mod(
-    modid = "idextender",
-    name = "ID Extender",
-    version = "1.0.0"
-)
+@Mod(modid = "idextender", name = "ID Extender", version = "1.0.0")
 public class IDExtender {
 
     public static String parentModId() {
